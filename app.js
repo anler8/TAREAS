d2c15f275480d4132f4f3438113360a35e49c107
anler8 <antolerma@gmail.com>
2025-08-20 20:07:16 -0300

    Add files via upload
    tarea numero adivinado

--- app.js
+++ app.js
@@ -1,29 +1,8 @@
-let saldo = 10000;
-let opcion = parseInt(prompt("Ingresa una opción:\n1 - Consultar saldo\n2 - Retirar\n3 - Depositar\n4 - Salir"));
-
-while (opcion !== 4) {
-    let importe;
-
-    if (opcion === 1) {
-        alert("Tu saldo es de $" + saldo);
-
-    } else if (opcion === 2) {
-        importe = parseInt(prompt("¿Cuánto querés retirar?"));
-
-        if (importe > saldo) {
-            alert("No disponés de fondos suficientes");
-        } else {
-            saldo = saldo - importe;
-            alert("Retiraste $" + importe + ". Nuevo saldo: $" + saldo);
-        }
-
-    } else if (opcion === 3) {
-        importe = parseInt(prompt("¿Cuánto querés depositar?"));
-        saldo = saldo + importe;
-        alert("Depositaste $" + importe + ". Nuevo saldo: $" + saldo);
-
-    } else {
-        alert("Por favor, ingresá una opción válida");
-    }
-    opcion = parseInt(prompt("Ingresa una opción:\n1 - Consultar saldo\n2 - Retirar\n3 - Depositar\n4 - Salir"));
+let numeroAdivinado = Math.floor (Math.random() *10) +1
+let intento;
+let cantidadIntentos= 0;
+while (intento !== numeroAdivinado) {
+    intento = parseInt(prompt("Adiviná el número del 1 al 10"));
+    cantidadIntentos++;
 }
+alert("Adivinaste y ganaste 1000 dolares! Lo hiciste en " + cantidadIntentos + " intentos.");
